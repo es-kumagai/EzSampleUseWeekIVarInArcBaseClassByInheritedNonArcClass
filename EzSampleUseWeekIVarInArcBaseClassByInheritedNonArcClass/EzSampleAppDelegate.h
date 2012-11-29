476dd54062aba655b2a70c71320d4cd6c3c0c8f1
Tomohiro Kumagai <tomohiro@ez-style.jp>
2012-11-29 14:09:10 +0900

    Initial Commit

---
+++ EzSampleUseWeekIVarInArcBaseClassByInheritedNonArcClass/EzSampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EzSampleAppDelegate.h
+//  EzSampleUseWeekIVarInArcBaseClassByInheritedNonArcClass
+//
+//  Created by 熊谷 友宏 on H.24/11/29.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EzSampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EzSampleUseWeekIVarInArcBaseClassByInheritedNonArcClass/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EzSampleUseWeekIVarInArcBaseClassByInheritedNonArcClass
+//
+//  Created by 熊谷 友宏 on H.24/11/29.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EzSampleAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([EzSampleAppDelegate class]));
+	}
+}
